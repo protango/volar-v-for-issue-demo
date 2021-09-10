3cba7c20774651e18e3944d19aa7b81538a8703a
protango <zacharymurray@hotmail.com>
2021-09-10 18:02:16 +1000

    initial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import MyComponent from './component.vue'
+  
+  export class TestClass {
+    private privateField = "";
+    public publicField = "";
+  }
+  
+  const objects = ref<TestClass[]>([]);
+</script>
+
+<template>
+  <MyComponent v-for="obj in objects" :key="obj.publicField" :test-prop="obj" />
+</template>
+
+
---
+++ src/component.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import { TestClass } from './App.vue';
+  defineProps<{ 
+    testProp: TestClass
+  }>();
+</script>
+<template>
+</template>
